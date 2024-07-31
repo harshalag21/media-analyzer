5958adcb6db7d40ae933139b4783d39c17189004
adityavkulkarni <adityavkulkarni0999@gmail.com>
2024-07-31 08:46:03 -0500

    added reddit config

--- config/parsedconfig.py
+++ config/parsedconfig.py
@@ -7,6 +7,12 @@ bootstrap_servers = config.get("KAFKA", "bootstrap_servers")
 
 news_api_key = config.get("NEWSAPI", "news_api_key")
 
+reddit_username = config.get("REDDIT", "username")
+reddit_password = config.get("REDDIT", "password")
+reddit_client_id = config.get("REDDIT", "client_id")
+reddit_client_secret = config.get("REDDIT", "client_secret")
+reddit_user_agent = config.get("REDDIT", "user_agent")
+
 input_topic = config.get("TOPICS", "input_topic")
 output_topic = config.get("TOPICS", "output_topic")
 
